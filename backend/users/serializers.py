7060163de9eec0fedc42739ed6bf87369426a893
armykongtap <35949144+armykongtap@users.noreply.github.com>
2020-04-17 13:44:13 +0700

    fix model, serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from users.models import UserModel
 
+# from django.contrib.auth.models import User
 
-class UserModelSerializer(serializers.HyperlinkedModelSerializer):
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('id','username')
+
+# class UserModelSerializer(serializers.ModelSerializer):
+#     user = UserSerializer(required=True)
+
+#     class Meta:
+#         model = UserModel
+#         fields = ('user', 'firstName','lastName','about')
+
+class UserModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
-        fields = ['userId', 'firstName', 'lastName', 'about']
+        fields = ('userId', 'firstName','lastName','about')
